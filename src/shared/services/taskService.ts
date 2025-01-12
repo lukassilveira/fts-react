e66995230228a90fcd099e55b518b8fbb939a0b1
Lukas Silveira <lukassilveira12@hotmail.com>
2025-01-12 00:32:36 -0300

    creating task service

---
+++ src/shared/services/taskService.ts
@@ -0,0 +1,31 @@
+// src/services/taskService.ts
+import { RootState } from "../../store";
+import { useSelector, useDispatch } from "react-redux";
+import { setFilter } from "../../features/filter/filterSlice.ts";
+import { setSortBy } from "../../features/sortBy/sortBySlice.ts";
+import { addTask } from "../../features/task/taskSlice.ts";
+
+export const useTaskService = () => {
+  const dispatch = useDispatch();
+
+  const filters = useSelector((state: RootState) => state.filters);
+
+  const handleFilterChange = (name: string, value: string) => {
+    dispatch(setFilter({ name, value }));
+  };
+
+  const handleSortChange = (value: string) => {
+    dispatch(setSortBy(value));
+  };
+
+  const handleCreateTask = (taskData: any) => {
+    dispatch(addTask(taskData));
+  };
+
+  return {
+    filters,
+    handleFilterChange,
+    handleSortChange,
+    handleCreateTask,
+  };
+};
